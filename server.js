b6f92495992ef1a47a084353c1310a75e5718fc2
Abubakr Abdugaffarov <abdugaffarovabubakr0@gmail.com>
2025-01-11 17:57:00 +0500

    express

--- server.js
+++ server.js
@@ -1,1 +1,58 @@
-const http  = require('http');
+// const express = require("express");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(express.json()); // JSON formatidagi ma'lumotlarni qabul qilish uchun
+// app.use(express.urlencoded({ extended: true })); // URL-encoded formatdagi ma'lumotlarni qabul qilish uchun
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("This is the about page");
+// });
+
+// app.post("/data", (req, res) => {
+//   const data = req.body;
+//   res.send(`You sent: ${JSON.stringify(data)}`);
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server has been started on port ${PORT}`);
+// });
+
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is the about page");
+});
+
+app.post("/data", (req, res) => {
+  const data = req.body;
+  res.send(`You sent: ${JSON.stringify(data)}`);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}`);
+});
+
